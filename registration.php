03acd4d56aef5482032ad16bd1910b064d04549c
sanjee <79707868+sanjeevan1@users.noreply.github.com>
2023-09-23 14:56:08 +0530

    Update registration.php
    git push -u origin ss

--- registration.php
+++ registration.php
@@ -1,14 +1,2 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title>User Login</title>
-</head> iam team leader
-<body>
-    <h2>User Login</h2>
-    <form method="post" action="authenticate.php">
-        <input type="email" name="email" placeholder="Email" required><br>
-        <input type="password" name="password" placeholder="Password" required><br>
-        <button type="submit">Login</button>
-    </form>
-</body>
-</html>
+its work
